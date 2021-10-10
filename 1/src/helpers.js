4d1c176654a887cb6ac0ddc3f5faf3bb93bb4391
Piotr <p.michalek@thesignalgroup.com>
2021-10-10 11:58:46 +0200

    Init with 7 tests

---
+++ 1/src/helpers.js
@@ -0,0 +1,24 @@
+const _ = require('lodash')
+
+function factorial(num) {
+    if (num === 0) {
+        return 1
+    }
+
+    return num * factorial(num - 1)
+}
+
+function checkArraysEquality(arr, arr2) {
+    return _.isEqual(arr, arr2)
+}
+
+function getValuesGreaterThan(arr, num) {
+    return arr.filter(n => n > num)
+}
+
+
+module.exports = {
+    factorial,
+    checkArraysEquality,
+    getValuesGreaterThan,
+}
---
+++ 1/test/helpers.test.js
@@ -0,0 +1,62 @@
+const { expect, it } = require("@jest/globals");
+const {
+    factorial,
+    checkArraysEquality,
+    getValuesGreaterThan,
+} = require("../src/helpers");
+
+
+describe('factorial', () => {
+    it('counts factorial properly', () => {
+        const result = factorial(3)
+        expect(result).toEqual(6)
+    })
+
+    it('counts factorial properly', () => {
+        const result = factorial(0)
+        expect(result).toEqual(1)
+    })
+})
+
+describe('checkArraysEquality', () => {
+    it('returns true if two arrays are equal', () => {
+        const arr = [1, 2]
+        const arr2 = [1, 2]
+        const result = checkArraysEquality(arr, arr2)
+
+        expect(result).toBeTruthy
+    })
+
+    it('returns false if two arrays are equal', () => {
+        const arr = [1, 2]
+        const arr2 = ['1', '2']
+        const result = checkArraysEquality(arr, arr2)
+
+        expect(result).toBeFalsy
+    })
+})
+
+describe('getValuesGreaterThan', () => {
+    it('returns empty array when cannot find grater values', () => {
+        const arr = [1, 2]
+        const result = getValuesGreaterThan(arr, 5)
+
+        expect(result).toStrictEqual([])
+    })
+
+    it('returns an array containing 5, 7, 9', () => {
+        const arr = [1, 2, 5, 7, 9]
+        const expectedResults = [5, 7, 9]
+        const result = getValuesGreaterThan(arr, 4)
+
+        expect(result).toEqual(expect.arrayContaining(expectedResults))
+    })
+
+    it('throws an error if array is not passed', () => {
+        expect(() => getValuesGreaterThan({}, 5)).toThrow()
+    })
+})
+
+
+
+
